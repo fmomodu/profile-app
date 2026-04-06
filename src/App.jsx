4c04ece9038addcc4c6ad745949a1be2dbceb26a
Fey Momodu <your_email@example.com>
2026-04-06 14:50:24 -0400

    Lab 5: interactive filtering and search

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import { useState } from 'react'
 import './App.css'
 
 function Header() {
   const title = "Favour's Profile";
-  const subtitle = "Lab 4: Props and Children:";
+  const subtitle = "Lab 5: ";
 
   return ( <header> 
    <h1>{title}</h1>
@@ -26,10 +27,18 @@ function Introduction() {
 }
 
 function App() {
+  // <select value={selectedTitle} onChange={(e) => setTitle(e.target.value)}>
+  // <input value={searchName} onChange={(e) => setName(e.target.value)} />
+
+  
+  // const [name , setName ]
+  const [selectedTitle, setTitle ] = useState(""); //dropdown
+  const [searchName, setName ] = useState(""); //search
   const cards = [
     {
       id: 1,
       name: "Favour", 
+      title: "Student",
       year: "Senior", 
       major: "IT", 
       isFeatured: true
@@ -37,30 +46,63 @@ function App() {
     {
       id: 2,
       name: "Ngozi", 
+      title: "Professor",
       year: "Junior", 
       major: "Cybersecurity", 
       isFeatured: false
     }
   ]
-  // array.map((item) => (
-  // <Component prop={item.prop} />
-// ))
 
-// key={card.id}
+  const filteredCards = cards.filter((card) => {
+    const matchesTitle = 
+    selectedTitle === "" || card.title === selectedTitle;
+
+    const matchesSearch = 
+    searchName === "" ||
+    card.name.toLowerCase().includes(searchName.toLowerCase());
+    return matchesTitle && matchesSearch;
+  });
+  
+
+
+  // array.map((item) => (
+  // <Component prop={item.prop} /> ))
   return (
     <div> 
       <Header />
       <Introduction />
+      <select value={selectedTitle} onChange={(e) => setTitle(e.target.value)}>
+        <option value="">All</option>
+        <option value= "Student">Student</option>
+        <option value="Professor">Professor</option>
+        <option value="Faculty">Faculty</option>
+        </select>
+
+      <input 
+        value={searchName} 
+        onChange={(e) => setName(e.target.value)} 
+        placeholder="Search by name"
+        />
+
+      <button onClick={() => {
+        setName("");
+        setTitle("");
+      }}>
+        Reset
+        </button>  
+
+
       <Section>
-          {cards.map((card) => (
-  <Card
-    key={card.id}
-    name={card.name}
-    year={card.year}
-    major={card.major}
-    isFeatured={card.isFeatured}
-  />
-))}
+        {filteredCards.map((card) => (
+          <Card
+            key={card.id}
+            name={card.name}
+            title={card.title}
+            year={card.year}
+            major={card.major}
+            isFeatured={card.isFeatured}
+          />
+        ))}
       </Section>
     
     </div>
@@ -76,14 +118,14 @@ function Section({ children}) {
   
 
 // function Component({ thing1, thing2 }) {
-// return <p>{thing1}</p>
-// }
-function Card({name, year, major, isFeatured}) {
+// return <p>{thing1}</p> }
+function Card({name,title, year, major, isFeatured}) {
   return (
     // condition ? "if true" : "if false"
     // with {variable}, without is text 
     <div className={isFeatured? "card featured" : "card"}> 
       <h3>{name}</h3>  
+      <p>{title}</p>
       <p>{year}</p>
       <p>{major}</p> 
     </div>
